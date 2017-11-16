19334d63d25d3596b0733de6a4534c241c3004c7
岳潇洋 <yuexy@yuexiaoyangdeMacBook-Pro-2.local>
2017-11-16 10:28:37 +0800

    注释

--- GMCache/GMCacheProvider.m
+++ GMCache/GMCacheProvider.m
@@ -26,6 +26,8 @@ static FMDatabaseQueue * ST_GMCache_DBQueue;
 
 @implementation GMCacheProvider
 
+#pragma -mark to initialize static varable and open database
+
 + (void)initialize {
     if (!ST_GMCache_MapTable) {
         ST_GMCache_MapTable=[NSCache new];
@@ -33,6 +35,8 @@ static FMDatabaseQueue * ST_GMCache_DBQueue;
         [self openDB];
     }
 }
+
+#pragma -mark get or save cache objects
 
 + (GMCache *)cacheWithIdentifier:(NSString *)identifier {
     if (![identifier isKindOfClass:[NSString class]] || identifier.length==0)return nil;
@@ -44,40 +48,6 @@ static FMDatabaseQueue * ST_GMCache_DBQueue;
     }
     UnLock();
     return cache;
-}
-
-+ (BOOL)openDB {
-    NSString * dbPath=[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
-    if (dbPath.length>0) {
-        dbPath=[dbPath stringByAppendingPathComponent:GMCache_Caches_Default];
-        ST_GMCache_DBQueue=[FMDatabaseQueue databaseQueueWithPath:dbPath];
-        if (ST_GMCache_DBQueue) {
-            NSString * initSql=@"CREATE TABLE IF NOT EXISTS gm_caches ( \
-            identifier varchar(50) PRIMARY KEY not null,\
-            directory INTEGER,\
-            subpath text,\
-            cache_age INTEGER,\
-            disk_limit INTEGER,\
-            mem_limit INTEGER,\
-            count_limit INTEGER)";
-            __block BOOL update=NO;
-            [ST_GMCache_DBQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
-                update=[db executeUpdate:initSql];
-                if (!update){*rollback=YES;return;};
-                FMResultSet * resultSet=[db executeQuery:@"select 1 from gm_caches where identifier=?",GMCache_Identifier_Default];
-                if (!resultSet || !resultSet.next) {
-                    [resultSet close];
-                    if (![db executeUpdate:@"insert into gm_caches (identifier,directory,subpath,cache_age,disk_limit,mem_limit,count_limit) values(?,?,?,?,?,?)",GMCache_Identifier_Default,@(GMCache_Dicrectory_Default),GMCache_SubPath_Default?GMCache_SubPath_Default:@"",@(GMCache_CacheAge_Default),@(GMCache_DiskLimit_Default),@(GMCache_MemLimit_Default),@(GMCache_CountLimit_Default)]) {
-                        *rollback=YES;
-                        return;
-                    }
-                }
-                update=YES;
-            }];
-            return update;
-        }
-    }
-    return NO;
 }
 
 + (BOOL)saveCache:(GMCache *)cache {
@@ -131,6 +101,42 @@ static FMDatabaseQueue * ST_GMCache_DBQueue;
         return cache;
     }
     return nil;
+}
+
+#pragma -mark open database and initialize default data
+
++ (BOOL)openDB {
+    NSString * dbPath=[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    if (dbPath.length>0) {
+        dbPath=[dbPath stringByAppendingPathComponent:GMCache_Caches_Default];
+        ST_GMCache_DBQueue=[FMDatabaseQueue databaseQueueWithPath:dbPath];
+        if (ST_GMCache_DBQueue) {
+            NSString * initSql=@"CREATE TABLE IF NOT EXISTS gm_caches ( \
+            identifier varchar(50) PRIMARY KEY not null,\
+            directory INTEGER,\
+            subpath text,\
+            cache_age INTEGER,\
+            disk_limit INTEGER,\
+            mem_limit INTEGER,\
+            count_limit INTEGER)";
+            __block BOOL update=NO;
+            [ST_GMCache_DBQueue inTransaction:^(FMDatabase * _Nonnull db, BOOL * _Nonnull rollback) {
+                update=[db executeUpdate:initSql];
+                if (!update){*rollback=YES;return;};
+                FMResultSet * resultSet=[db executeQuery:@"select 1 from gm_caches where identifier=?",GMCache_Identifier_Default];
+                if (!resultSet || !resultSet.next) {
+                    [resultSet close];
+                    if (![db executeUpdate:@"insert into gm_caches (identifier,directory,subpath,cache_age,disk_limit,mem_limit,count_limit) values(?,?,?,?,?,?)",GMCache_Identifier_Default,@(GMCache_Dicrectory_Default),GMCache_SubPath_Default?GMCache_SubPath_Default:@"",@(GMCache_CacheAge_Default),@(GMCache_DiskLimit_Default),@(GMCache_MemLimit_Default),@(GMCache_CountLimit_Default)]) {
+                        *rollback=YES;
+                        return;
+                    }
+                }
+                update=YES;
+            }];
+            return update;
+        }
+    }
+    return NO;
 }
 
 @end
